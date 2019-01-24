f858ce33d0c239d1818cc8c72410d55318fba9fc
yufan <syf941020@gmail.com>
2019-01-24 21:32:42 +1100

    Initial Commit

---
+++ ChatOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatOne
+//
+//  Created by 沈宇帆 on 24/1/19.
+//  Copyright © 2019 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
